2e9dab6b62b0fa5b464f925fae92580d20c28e81
Bryan Andrade <bryand1@gmail.com>
2019-07-03 10:25:14 -0700

    Update version

--- icd10/__version__.py
+++ icd10/__version__.py
@@ -2,7 +2,7 @@ __title__ = 'icd10-cm'
 __description__ = ('ICD-10 codes for diseases, signs and symptoms, abnormal findings, '
                    'complaints, social circumstances, and external causes of injury or disease')
 __url__ = 'https://github.com/bryand1/icd10-cm'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Bryan Andrade'
 __author_email__ = 'me@bryanandrade.com'
 __license__ = 'MIT'
