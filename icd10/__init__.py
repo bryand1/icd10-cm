93623cf911b1004a5510ccb0b75d62c28a3c882a
Felipe Kafuri <felipe11.rk@gmail.com>
2022-10-05 10:11:10 -0300

    added D5912

--- icd10/__init__.py
+++ icd10/__init__.py
@@ -12,53 +12,33 @@ import os
 from typing import Optional
 
 here = os.path.dirname(os.path.abspath(__file__))
-with gzip.open(os.path.join(here, "icd10.json.gz")) as fh:
+with gzip.open(os.path.join(here, 'icd10.json.gz')) as fh:
     codes = json.load(fh)
 
 # https://en.wikipedia.org/wiki/ICD-10#List
 chapters = [
-    ("I", "A00-B99", "Certain infectious and parasitic diseases"),
-    ("II", "C00-D48", "Neoplasms"),
-    (
-        "III",
-        "D50-D89",
-        "Diseases of the blood and blood-forming organs and certain disorders involving the immune mechanism",
-    ),
-    ("IV", "E00-E90", "Endocrine, nutritional and metabolic diseases"),
-    ("V", "F00-F99", "Mental and behavioural disorders"),
-    ("VI", "G00-G99", "Diseases of the nervous system"),
-    ("VII", "H00-H59", "Diseases of the eye and adnexia"),
-    ("VIII", "H60-H95", "Diseases of the ear and mastoid process"),
-    ("IX", "I00-I99", "Diseases of the circulatory system"),
-    ("X", "J00-J99", "Diseases of the respiratory system"),
-    ("XI", "K00-K93", "Diseases of the digestive system"),
-    ("XII", "L00-L99", "Diseases of the skin and subcutaneous tissue"),
-    ("XIII", "M00-M99", "Diseases of the musculoskeletal system and connective tissue"),
-    ("XIV", "N00-N99", "Diseases of the genitourinary system"),
-    ("XV", "O00-O99", "Pregnancy, childbirth and the puerperium"),
-    ("XVI", "P00-P96", "Certain conditions originating in the perinatal period"),
-    (
-        "XVII",
-        "Q00-Q99",
-        "Congenital malformations, deformations and chromosomal abnormalities",
-    ),
-    (
-        "XVIII",
-        "R00-R99",
-        "Symptoms, signs and abnormal clinical and laboratory findings, not elsewhere classified",
-    ),
-    (
-        "XIX",
-        "S00-T98",
-        "Injury, poisoning and certain other consequences of external causes",
-    ),
-    ("XX", "V01-Y98", "External causes of morbidity and mortality"),
-    (
-        "XXI",
-        "Z00-Z99",
-        "Factors influencing health status and contact with health services",
-    ),
-    ("XXII", "U00-U99", "Codes for special purposes"),
+    ('I', 'A00-B99', 'Certain infectious and parasitic diseases'),
+    ('II', 'C00-D48', 'Neoplasms'),
+    ('III','D50-D89','Diseases of the blood and blood-forming organs and certain disorders involving the immune mechanism'),
+    ('IV', 'E00-E90', 'Endocrine, nutritional and metabolic diseases'),
+    ('V', 'F00-F99', 'Mental and behavioural disorders'),
+    ('VI', 'G00-G99', 'Diseases of the nervous system'),
+    ('VII', 'H00-H59', 'Diseases of the eye and adnexia'),
+    ('VIII', 'H60-H95', 'Diseases of the ear and mastoid process'),
+    ('IX', 'I00-I99', 'Diseases of the circulatory system'),
+    ('X', 'J00-J99', 'Diseases of the respiratory system'),
+    ('XI', 'K00-K93', 'Diseases of the digestive system'),
+    ('XII', 'L00-L99', 'Diseases of the skin and subcutaneous tissue'),
+    ('XIII', 'M00-M99', 'Diseases of the musculoskeletal system and connective tissue'),
+    ('XIV', 'N00-N99', 'Diseases of the genitourinary system'),
+    ('XV', 'O00-O99', 'Pregnancy, childbirth and the puerperium'),
+    ('XVI', 'P00-P96', 'Certain conditions originating in the perinatal period'),
+    ('XVII','Q00-Q99','Congenital malformations, deformations and chromosomal abnormalities'),
+    ('XVIII','R00-R99','Symptoms, signs and abnormal clinical and laboratory findings, not elsewhere classified'),
+    ('XIX','S00-T98','Injury, poisoning and certain other consequences of external causes'),
+    ('XX', 'V01-Y98', 'External causes of morbidity and mortality'),
+    ('XXI','Z00-Z99','Factors influencing health status and contact with health services'),
+    ('XXII', 'U00-U99', 'Codes for special purposes'),
 ]
 
 
@@ -95,12 +75,12 @@ class ICD10:
 
     def __str__(self):
         if len(self.code) > 3:
-            return self.code[:3] + "." + self.code[3:]
+            return self.code[:3] + '.' + self.code[3:]
         else:
             return self.code
 
     def __repr__(self):
-        return "<ICD10: %s>" % self
+        return '<ICD10: %s>' % self
 
     def __hash__(self):
         return hash(self.code)
@@ -116,24 +96,24 @@ class ICD10:
 
 def exists(s: str) -> bool:
     """
-    >>> exists("T50.B")
+    >>> exists('T50.B')
     True
-    >>> exists("A99.8")
+    >>> exists('A99.8')
     False
     """
     if not s:
         return False
-    return bool(codes.get(s.replace(".", ""), False))
+    return bool(codes.get(s.replace('.', ''), False))
 
 
 def find(s: str) -> Optional[ICD10]:
     """
-    >>> find("A02.1")
+    >>> find('A02.1')
     <ICD10: A02.1>
     """
     if not s:
         return
-    k = s.replace(".", "")
+    k = s.replace('.', '')
     v = codes.get(k)
     if v is None:
         return
@@ -142,8 +122,8 @@ def find(s: str) -> Optional[ICD10]:
 
 
 def in_chapter(block: str, icd10: str) -> bool:
-    alpha, numeric = ord(icd10[0]), int(icd10[1:3].lstrip("0"))
-    sblock, eblock = block.split("-")  # A00-B99
-    salpha, snumeric = ord(sblock[0]), int(sblock[1:].lstrip("0") or 0)
-    ealpha, enumeric = ord(eblock[0]), int(eblock[1:].lstrip("0") or 0)
+    alpha, numeric = ord(icd10[0]), int(icd10[1:3].lstrip('0'))
+    sblock, eblock = block.split('-')  # A00-B99
+    salpha, snumeric = ord(sblock[0]), int(sblock[1:].lstrip('0') or 0)
+    ealpha, enumeric = ord(eblock[0]), int(eblock[1:].lstrip('0') or 0)
     return salpha <= alpha <= ealpha and snumeric <= numeric <= enumeric
