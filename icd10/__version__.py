1046dbe05ff874053100fa04a2784d24694636fc
Felipe Ramos Kafuri <felipe11.rk@gmail.com>
2022-10-06 13:34:18 -0300

    Update __version__.py

--- icd10/__version__.py
+++ icd10/__version__.py
@@ -3,7 +3,7 @@ __description__ = ('ICD-10 codes for diseases, signs and symptoms, abnormal find
                    'complaints, social circumstances, and external causes of injury or disease')
 __url__ = 'https://github.com/bryand1/icd10-cm'
 __version__ = '0.0.5'
-__author__ = 'Bryan Andrad'
+__author__ = 'Bryan Andrade'
 __author_email__ = 'me@bryanandrade.com'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019 Bryan Andrade'
